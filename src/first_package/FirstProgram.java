d4dcfce78e1d2dc3bcd13d858ddca3f6893fa5bb
Tech Global <techglobal@Techs-iMac.local.tld>
2023-01-28 16:28:16 -0800

    first commit

---
+++ src/first_package/FirstProgram.java
@@ -0,0 +1,12 @@
+package first_package;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("This is TechGlobal");
+        System.out.println("I am learning Java");
+        System.out.println("My name is Tech");
+
+
+        // More practices
+    }
+}
