e7bd340f5e28c1d9992684ff5830f2a9f16ff7a6
akinkayagithub <akinkaya.b@gmail.com>
2023-02-14 19:04:20 -0600

    added conditional statements example

---
+++ src/conditional_statements/Exercise04_RetirementAge.java
@@ -0,0 +1,52 @@
+package conditional_statements;
+
+import java.util.Scanner;
+
+public class Exercise04_RetirementAge {
+    public static void main(String[] args) {
+        /*
+        Write a Java program that asks user to enter their age
+        And print “It is your time to get retired!” if their age is more than or equals 55
+        else calculate and tell user how many years later they can be retired
+
+        EXAMPLE PROGRAM 1
+        Program: Please enter your age?
+        User: 60
+        Program: It is your time to get retired!
+
+
+        EXAMPLE PROGRAM 1
+        Program: Please enter your age?
+        User: 20
+        Program: You have 35 years to be retired
+
+
+        EDGE CASES
+        less than 55
+        55
+        more than 55
+
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your age?");
+        int age = input.nextInt();
+
+        if(age >= 55){
+            System.out.println("It is your time to get retired!");
+        }
+        else{
+            int ageLeft = 55 - age;
+
+            if(ageLeft == 1){
+                System.out.println("You have 1 year to be retired");
+            }
+            else{
+                System.out.println("You have " + ageLeft + " years to be retired");
+            }
+        }
+
+        System.out.println("End of the program");
+    }
+}
